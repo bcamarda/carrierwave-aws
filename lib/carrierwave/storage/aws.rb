07b1c8fd1a6518b745a535de0591cd61b5192f51
Brett Camarda <bcamarda@gmail.com>
2015-03-26 15:47:04 -0700

    aws capitalization in v2 sdk

--- lib/carrierwave/storage/aws.rb
+++ lib/carrierwave/storage/aws.rb
@@ -22,7 +22,7 @@ module CarrierWave
 
       def connection
         @connection ||= begin
-          self.class.connection_cache[credentials] ||= AWS::S3::Client.new(*credentials)
+          self.class.connection_cache[credentials] ||= Aws::S3::Client.new(*credentials)
         end
       end
 
